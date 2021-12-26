2327e900a01e1f3f22d868ccc88f73790d2b0e08
Clair Luis Jevinski <cl.jevinski@hotmail.com>
2021-12-26 15:04:44 -0300

    Obtendo um usuário por id

--- src/main/java/com/clj/course/services/UserService.java
+++ src/main/java/com/clj/course/services/UserService.java
@@ -1,22 +1,29 @@
 package com.clj.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.clj.course.domain.User;
 import com.clj.course.repository.UserRepository;
+import com.clj.course.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
 
 	public List<User> findAll() {
-		
+
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
